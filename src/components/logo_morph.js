54a8f33bc67935f7e12e600172d1cc6d6e2ecaf9
Alex Gieche <alex.gieche@gmail.com>
2019-06-03 00:04:50 +0200

    Add back animation on morph logo.

--- src/components/logo_morph.js
+++ src/components/logo_morph.js
@@ -27,12 +27,14 @@ class LogoMorph extends React.Component {
     this.logoAnimationSvgStep3 = null;
     this.logoAnimationSvgStep4 = null;
     this.logoAnimationSvgStep5 = null;
+    this.logoAnimationSvgBack = null;
 
     this.logoAnimationStep1 = null;
     this.logoAnimationStep2 = null;
     this.logoAnimationStep3 = null;
     this.logoAnimationStep4 = null;
     this.logoAnimationStep5 = null;
+    this.logoAnimationBack = null;
 
     this.timeLine = new TimelineMax({paused: true});
 
@@ -81,13 +83,14 @@ class LogoMorph extends React.Component {
 
   handleEnter() {
     this.timeLine
-      .to(this.logoAnimationStep1, 0.5, {morphSVG:{scale: 1, shape:this.logoAnimationStep4, ease: Elastic.easeOut}, ease: Bounce.ease})
+      .to(this.logoAnimationStep1, 1, {rotation: '+=90', scale: 0.8, x: '-=80', ease: Elastic.easeOut})
+      .to(this.logoAnimationStep1, 0.3, {morphSVG:{scale: 1, shape:this.logoAnimationBack, ease: Elastic.easeOut}, ease: Bounce.ease}, '-=1')
   }
 
   handleLeave() {
     this.timeLine
-      .to(this.logoAnimationStep1, 1, {fill: "#00df7a", scale: 0.8, ease: Elastic.easeOut.config(1, 0.3)})
-      .to(this.logoAnimationStep1, 1, {morphSVG:{shape:this.logoAnimationStep5, ease: Elastic.easeOut}}, '-=1')
+      .to(this.logoAnimationStep1, 1, {rotation: '-=90', scale: 1, x: '+=80', fill: "#00df7a", scale: 0.8, ease: Elastic.easeOut.config(1, 0.3)})
+      .to(this.logoAnimationStep1, 0.3, {morphSVG:{shape:this.logoAnimationStep5, ease: Elastic.easeOut}}, '-=1')
 
   }
 
@@ -146,6 +149,16 @@ class LogoMorph extends React.Component {
               <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
                   <g id="step-5">
                       <path ref={path => this.logoAnimationStep5 = path} id='logo-animation-step-5' d="M0.00238855775,41.7290146 C-0.143704386,42.0991168 6.4304781,42.4010422 14.611683,42.4010422 C22.7977576,42.4010422 44.5607365,42.6445304 62.9733173,42.9367163 L96.4529503,43.4772602 L96.4529503,58.3446521 L96.4529503,73.2169139 L74.3636972,73.4311835 L52.2695742,73.6454532 L40.2802133,90.2854395 C33.6865517,99.4357276 26.7666159,109.072992 24.9063658,111.702665 L21.526749,116.479904 L59.18951,116.16337 L96.8571408,115.851705 L96.603913,131.556696 C96.2630295,153.00801 98.3278098,164.305865 105.330532,179.251173 C128.76384,229.249048 192.007475,244.218705 237.452121,210.524802 C248.42857,202.387425 255.124497,194.863639 272.490078,171.181972 L288.998581,148.65931 L296.410363,158.759202 C300.486356,164.315604 313.683419,182.387301 325.736087,198.925023 L347.650028,228.99582 L373.825014,229.034778 C388.224909,229.054257 400,228.888685 400,228.659806 C400,228.435797 389.125815,213.548926 375.836227,195.589234 C323.237897,124.495537 315.193046,113.499608 315.363488,112.939585 C315.460883,112.618181 333.074822,88.2157895 354.511527,58.7050147 C375.943362,29.19424 394.078366,4.13930009 394.803961,3.02899371 L396.128537,1.00804131 L370.270086,1.0372599 L344.406765,1.06647849 L316.858505,39.2697834 C291.842524,73.9668576 289.21772,77.3318651 288.224288,75.9634612 C287.620438,75.1356012 274.998007,57.9404617 260.169573,37.7504168 L233.210555,1.04212967 L207.084267,1.02265061 L180.957979,1.00804131 L210.692763,41.7923216 C227.050303,64.2224583 245.467753,89.4867981 251.628006,97.9309703 L262.818725,113.285339 L260.934126,115.788398 C259.896867,117.161671 250.814755,129.60879 240.748951,143.448662 C221.985747,169.248676 217.286424,174.605417 208.408843,180.34687 C186.655604,194.401011 155.859211,186.151629 144.249692,163.15173 C139.569848,153.879698 139.652634,155.369846 139.12183,75.325522 C138.853992,34.7360324 138.542328,1.43658062 138.435193,1.32457603 C138.328058,1.2174412 113.769834,0.8716879 83.8646083,0.564892718 L29.4936842,0 L14.8843898,20.5309284 C6.84927788,31.8190432 0.153351267,41.3589125 0.00238855775,41.7290146 Z" id="path1-copy-10" fill="#00df7a"></path>
+                  </g>
+              </g>
+          </svg>
+
+          <svg ref={svg => this.logoAnimationSvgBack = svg} id='logo-animation-svg-back' className='morph-logo__target' width="400px" height="230px" viewBox="0 0 400 230" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+              <title>Back</title>
+              <desc>Created with Sketch.</desc>
+              <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                  <g id="back">
+                      <path ref={path => this.logoAnimationBack = path} id='logo-animation-back' transform="translate(115.316527, 115.000000) rotate(90.000000) translate(-115.316527, -115.000000) " d="M115.487506 228.046758 230.316527 2.38847327 189.722082 1.95324193 114.074227 146.955228 41.2987693 3.00803456 0.31652689 2.74993702z" id="path1-copy-10" fill="#00df7a"></path>
                   </g>
               </g>
           </svg>
